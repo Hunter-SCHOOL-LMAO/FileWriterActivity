c049930eec54322be274e74202f8e7ca4b8b5c31
Hunter Madden <hmadden1@hwemail.com>
2025-09-18 21:22:43 -0700

    (MFW-0.5.1): Added HashFile Method
    Added a new function called hashFile that uses SHA256 to convert an entire file into a hash function.

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,7 +1,9 @@
 import java.io.*;
 import java.nio.file.*;
 import java.nio.charset.StandardCharsets;
-
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.math.BigInteger;
 public class MyFileWriter {
     public static void main(String[] args) {
         String data = "Hello, World!";
@@ -48,7 +50,7 @@ public class MyFileWriter {
     }
     public void makeSecretFile(){
         try{
-            Files.write(Path.get(".secretFiles.txt"), "IT'S WANI WEDNESDAY".getBytes(StandardCharsets.UTF_8));
+            Files.write(Paths.get(".secretFiles.txt"), "IT'S WANI WEDNESDAY".getBytes(StandardCharsets.UTF_8));
         }
         catch (IOException e){
             e.printStackTrace();
@@ -56,7 +58,7 @@ public class MyFileWriter {
     }
     public void makePublicFileInSecretFolder(){
          try{
-            Files.write(Path.get("/.ultraUnacessable/lessSecretFiles.txt"), "IT'S WANI WEDNESDAY".getBytes(StandardCharsets.UTF_8));
+            Files.write(Paths.get("/.ultraUnacessable/lessSecretFiles.txt"), "IT'S WANI WEDNESDAY".getBytes(StandardCharsets.UTF_8));
         }
         catch (IOException e){
             e.printStackTrace();
@@ -64,7 +66,12 @@ public class MyFileWriter {
     }
     // Calculate and print the file size using the File class
     private static void printFileSize(String fileName) {
-        System.out.println(File.size(Path.get(fileName)));
+        try{
+        System.out.println(Files.size(Paths.get(fileName)));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
     /**
     * Reads a text file and returns its contents as a string.
@@ -74,7 +81,29 @@ public class MyFileWriter {
     * @throws IOException if an I/O error occurs
     */
     public static String stringify(String filePath) throws IOException {
-        return File.readString(Path.get(filePath));
+        return Files.readString(Paths.get(filePath));
     }
+    /*
+     * Kudos to GeeksForGeeks.org for teaching me how to use MessgeDigest and best practices for 
+     * converting stuff into Hexidecimal
+     */
+    public String hashFile(String filePath){
+        try{
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] intermediaryArray = md.digest(Files.readString(Paths.get(filePath), StandardCharsets.UTF_8).getBytes(StandardCharsets.UTF_8));
+        BigInteger hashResult = new BigInteger(1, intermediaryArray);
+        StringBuilder output = new StringBuilder(hashResult.toString(16));
+        while (output.length() < 64)
+        {
+            output.insert(0, '0');
+        }
 
+        return output.toString();
+    } catch(NoSuchAlgorithmException e){
+            e.printStackTrace();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
